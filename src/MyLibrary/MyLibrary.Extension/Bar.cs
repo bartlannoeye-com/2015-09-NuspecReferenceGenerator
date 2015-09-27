9c274d876c5a2ba17cade891aee974e764111f41
bartlannoeye <bart.lannoeye@gmail.com>
2015-09-27 13:13:08 +0200

    Added project showing a reference issue with Nuspec.ReferenceGenerator

---
+++ src/MyLibrary/MyLibrary.Extension/Bar.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace MyLibrary.Extension
+{
+    public class Bar
+    {
+        public string Title { get; set; }
+        public Foo Foo { get; set; }
+
+        public string Serialize(Foo foo)
+        {
+            return JsonConvert.SerializeObject(foo);
+        }
+    }
+}
---
+++ src/MyLibrary/MyLibrary/Foo.cs
@@ -0,0 +1,7 @@
+﻿namespace MyLibrary
+{
+    public class Foo
+    {
+        public string Name { get; set; }
+    }
+}
